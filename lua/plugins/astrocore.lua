e36c3db0927cd33c9e9216041593c30f027c66eb
tobiaspickel <tobias.pickel@thomann.io>
2024-12-02 10:19:57 +0100

    feat: spell checking

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -27,9 +27,11 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
+        relativenumber = true, -- sets vim.opt.relative number
         number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
+        spell = true, -- sets vim.opt.spell
+        spelllang = "en_us",
+        spelloptions = "camel",
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
         showtabline = 0,
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -15,6 +15,7 @@ return {
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
+        ["@spell"] = { link = "SpellBad" },
         -- Normal = { bg = "#000000" },
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
