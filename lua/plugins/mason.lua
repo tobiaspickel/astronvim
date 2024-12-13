6ffc5fdf0a785e06e3ebbbe5d17b89c89fb0d53a
tobiaspickel <tobias.pickel@thomann.io>
2024-12-13 10:27:17 +0100

    fix: remove emmet and add tailwindls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason plugins
 
@@ -8,12 +8,10 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        -- add more arguments for adding more language servers
-      },
-    },
+    opts = function(_, opts)
+      opts.ensure_installed = vim.tbl_filter(function(s) return s ~= "emmet_ls" end, opts.ensure_installed)
+      table.insert(opts.ensure_installed, "tailwindcss")
+    end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
@@ -26,14 +24,14 @@ return {
       },
     },
   },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = {
-      ensure_installed = {
-        "python",
-        -- add more arguments for adding more debuggers
-      },
-    },
-  },
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   -- overrides `require("mason-nvim-dap").setup(...)`
+  --   opts = {
+  --     ensure_installed = {
+  --       "python",
+  --       -- add more arguments for adding more debuggers
+  --     },
+  --   },
+  -- },
 }
