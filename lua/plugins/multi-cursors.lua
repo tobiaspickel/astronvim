d0128e0a3d12174a97de930ef4446c155d900d06
tobiaspickel <tobias.pickel@thomann.io>
2024-12-20 09:22:48 +0100

    feat: add multicursor

---
+++ lua/plugins/multi-cursors.lua
@@ -0,0 +1,38 @@
+return {
+  "brenton-leighton/multiple-cursors.nvim",
+  cmd = {
+    "MultipleCursorsAddDown",
+    "MultipleCursorsAddUp",
+    "MultipleCursorsMouseAddDelete",
+    "MultipleCursorsAddMatches",
+    "MultipleCursorsAddMatchesV",
+    "MultipleCursorsAddJumpNextMatch",
+    "MultipleCursorsJumpNextMatch",
+    "MultipleCursorsLock",
+  },
+  dependencies = {
+    "AstroNvim/astrocore",
+    opts = function(_, opts)
+      local maps = opts.mappings
+      for lhs, map in pairs {
+        ["<leader>j"] = { "<Cmd>MultipleCursorsAddDown<CR>", desc = "Add cursor down" },
+        ["<leader>k"] = { "<Cmd>MultipleCursorsAddUp<CR>", desc = "Add cursor up" },
+      } do
+        maps.n[lhs] = map
+        maps.i[lhs] = map
+      end
+      local prefix = "<leader>m"
+      for lhs, map in pairs {
+        [prefix .. "a"] = { "<Cmd>MultipleCursorsAddMatches<CR>", desc = "Add cursor matches" },
+        [prefix .. "A"] = { "<Cmd>MultipleCursorsAddMatchesV<CR>", desc = "Add cursor matches in previous visual area" },
+        [prefix .. "j"] = { "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", desc = "Add cursor and jump to next match" },
+        [prefix .. "J"] = { "<Cmd>MultipleCursorsJumpNextMatch<CR>", desc = "Move cursor to next match" },
+        [prefix .. "l"] = { "<Cmd>MultipleCursorsLock<CR>", desc = "Lock virtual cursors" },
+      } do
+        maps.n[lhs] = map
+        maps.x[lhs] = map
+      end
+    end,
+  },
+  opts = {},
+}
